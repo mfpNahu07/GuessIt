3ae65b02021b3ff6d17643aa43c2045b2fbabf01
unknown <nahuelji.773@gmail.com>
2023-03-14 09:58:46 -0300

    ScoreViewModelFactory added

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -19,7 +19,7 @@ class GameViewModel : ViewModel() {
         const val ONE_SECOND = 1000L
 
         // This is the total time of the game
-        const val COUNTDOWN_TIME = 60000L
+        const val COUNTDOWN_TIME = 10000L
 
     }
 
